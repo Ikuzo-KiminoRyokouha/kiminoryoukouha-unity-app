bff20d75fad114dc0b346d853c1832128d3ae0fa
SHIN_MINGYU <smg20004@naver.com>
2023-03-06 19:33:03 +0900

    ➖ minus : ArcGISMap delete

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public void Start(){
+
+    }
+
+    public void Update(){
+        
+    }
+
+    private async void Initialize()
+    {
+      
+    }
+}
